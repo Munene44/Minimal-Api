98607de9c21afd1dd40dc8ede75e9e1bc7f5ceec
Munene44 <karvinmitchar@gmail.com>
2023-11-01 09:02:21 +0300

    add routes and path

--- obj/Debug/net6.0/PizzaStore.AssemblyInfo.cs
+++ obj/Debug/net6.0/PizzaStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PizzaStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ceab35f972dd02a771e18a32e3d6c8bb3b2e3fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("PizzaStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PizzaStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
